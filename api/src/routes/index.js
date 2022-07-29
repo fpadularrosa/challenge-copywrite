5d096631ce4eb1fb69127bc0e6288b20a1dfdd18
unknown <francofyzz15@gmail.com>
2022-07-28 22:38:19 -0300

    pequeño cambio a la respuesta de la API

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,11 +10,11 @@ router.get('/reverse', (req, res) => {
     const normalized = text.toLowerCase().split('').reverse().join('')
     const palindrome = normalized === normalized.split('').reverse().join('')
 
-    text === '' && res.status(400).json({ success: false, error: 'text is empty' })
-    Number(text) && res.status(400).json({ success: false, error: 'number is invalid' })
+    text === '' && res.status(400).json({ error: 'text is empty' })
+    Number(text) && res.status(400).json({ error: 'number is invalid' })
     return res.status(200).json({ inverted: text.split('').reverse().join(''), palindrome })
   } catch (error) {
-    res.status(404).json({ success: false, error: error.message })
+    res.status(404).json({ error: error.message })
   }
 })
 
